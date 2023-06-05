0d41596e8fece7568e485ca0b05b8985813523e9
Eyüp Kahraman <96554015+EyupKhrmn@users.noreply.github.com>
2023-06-05 10:42:19 +0300

    P2P Message Type & Publish/Subscriber Message Type eklendi testleri yapıldı.

--- RabbitMQ.MessageType.Consumer/Program.cs
+++ RabbitMQ.MessageType.Consumer/Program.cs
@@ -1,3 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
-Console.WriteLine("Hello, World!");
+#region Connection
+
+//bağlantı oluşturma
+ConnectionFactory factory = new ();
+factory.Uri = new ("amqps://zsnlveev:cUZKbiRaHqFpwyS6n8BifgxqwlebTcqa@toad.rmq.cloudamqp.com/zsnlveev");
+
+//Bağlantı aktifleştirme ve kanal açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+#endregion
+
+#region P2P (Ponint-To-Point) Type
+
+// string queueName = "example-p2p";
+//
+// channel.QueueDeclare(
+//     queueName,
+//     durable: false,
+//     exclusive: false,
+//     autoDelete: false);
+//
+// EventingBasicConsumer consumer = new(channel);
+//
+// channel.BasicConsume(
+//     queue: queueName,
+//     autoAck: false,
+//     exclusive: false,
+//     consumer: consumer);
+//
+// consumer.Received += (sender, e) =>
+// {
+//     string message = Encoding.UTF8.GetString(e.Body.Span);
+//     Console.WriteLine(message);
+// };
+
+#endregion
+
+#region Publish / Subscribe (Puv/Sub) Type
+
+string exchangeName = "example-pub/sub";
+
+channel.ExchangeDeclare(
+    exchange: exchangeName,
+    type: ExchangeType.Fanout);
+
+string queueName = "example-pub/sub";
+
+channel.QueueBind(
+    queue: queueName,
+    exchange: exchangeName,
+    routingKey: string.Empty);
+
+EventingBasicConsumer consumer = new(channel);
+
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: false,
+    consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+    #endregion
+
+Console.Read();
--- RabbitMQ.MessageType.Publisher/Program.cs
+++ RabbitMQ.MessageType.Publisher/Program.cs
@@ -1,3 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Globalization;
+using System.Text;
+using RabbitMQ.Client;
 
-Console.WriteLine("Hello, World!");
+#region Connection
+
+//Bağlantı Oluşturma işlemi
+ConnectionFactory factory = new();
+factory.Uri = new ("amqps://zsnlveev:cUZKbiRaHqFpwyS6n8BifgxqwlebTcqa@toad.rmq.cloudamqp.com/zsnlveev");
+
+
+//Bağlantıyı aktifleştirme ve kanal açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+#endregion
+
+#region P2P (Ponint-To-Point) Type
+
+//string queueName = "eample-p2p";
+
+// channel.QueueDeclare(
+//     queueName,
+//     durable: false,
+//     exclusive: false,
+//     autoDelete: false);
+//
+// byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+//
+// channel.BasicPublish(
+//     exchange: string.Empty,
+//     routingKey: queueName,
+//     body: message);
+
+#endregion
+
+#region Publish / Subscribe (Puv/Sub) Type
+
+string exchangeName = "example-pub/sub";
+
+channel.ExchangeDeclare(
+    exchange: exchangeName,
+    type: ExchangeType.Fanout);
+
+for (int i = 0; i < 101; i++)
+{
+    byte[] body = Encoding.UTF8.GetBytes("Merhaba " + i);
+
+    channel.BasicPublish(
+        exchange: exchangeName,
+        routingKey: string.Empty,
+        body: body);
+}
+
+#endregion
+
+
+Console.Read();
